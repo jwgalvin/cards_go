7f01796f78f48af444b7f6613bbca4e96841ebb1
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-13 13:14:31 -0600

    use new deck creation function

--- deck.go
+++ deck.go
@@ -4,6 +4,23 @@ import "fmt"
 
 type deck []string
 
+
+//no receiver as this creates the deck
+
+func newDeck() deck {   
+	cards := deck{}
+	
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	
+	for _, suit :=  range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
