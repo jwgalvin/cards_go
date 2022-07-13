54b8fce0684153f64f5b0e635da1ae5137e05571
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-13 12:34:22 -0600

    separate to deck Class

---
+++ deck.go
@@ -0,0 +1,11 @@
+package main
+// create a new 'type' of called deck. it is a slice of strings. Similar to a class
+
+
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+		}
+}
