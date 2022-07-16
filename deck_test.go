4689b64643647917367a3109101be3f15eb91e2a
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-15 20:27:17 -0600

    got test to work

--- deck_test.go
+++ deck_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 	if len(d) != 2000{
-		t.Errorf("Expected deck 20, but got", len(d))
+		t.Errorf("Expected deck 20, but got %v", len(d))
 	}
 }
