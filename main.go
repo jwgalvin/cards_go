59b4f2dbaa636cbdd4891af637ec434fb92b60f9
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-15 19:46:03 -0600

    randomized in same fashion.

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ package main
 func main(){
 	// cards := newDeck()	
 	// cards.saveToFile("my_cards")
-	cards := newDeckFromFile("my_cards")
+	// cards := newDeckFromFile("my_cards")
+	// cards.print()
+	
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 }
 
