5da272be197993ab897a6acb71983dbd3cc572dc
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-16 13:30:24 -0600

    add more tests

--- deck_test.go
+++ deck_test.go
@@ -7,4 +7,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16{
 		t.Errorf("Expected deck 16, but got %v", len(d))
 	}
+	
+	if d[0] != "Ace of Spades"{
+		t.Errorf("Expected first card to be Ace of Spades, got %v", d[0])
+	}
+	
+	if d[len(d)-1] != "Four of Clubs"{
+		t.Errorf("Expected last card to be Four of Clubs, got %v", d[len(d)-1])
+		}
 }
