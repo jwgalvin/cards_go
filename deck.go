872fc8780cd86b3fbbba0da4e8f23c2936f060e2
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-15 19:32:27 -0600

    use file from hdd

--- deck.go
+++ deck.go
@@ -4,6 +4,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -42,4 +43,16 @@ func (d deck) toString() string {
 
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// log the error and call to newDeck()
+		// log the error and quit the program
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 
 func main(){
-	cards := newDeck()	
-	cards.saveToFile("my_cards")
+	// cards := newDeck()	
+	// cards.saveToFile("my_cards")
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
 
 
