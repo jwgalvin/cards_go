a406015c293a5a31706aaf9adfc1f397ac5a1a62
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-16 13:42:35 -0600

    added test test file and remove said file

--- deck_test.go
+++ deck_test.go
@@ -1,9 +1,13 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
+	
 	if len(d) != 16{
 		t.Errorf("Expected deck 16, but got %v", len(d))
 	}
@@ -15,4 +19,20 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs"{
 		t.Errorf("Expected last card to be Four of Clubs, got %v", d[len(d)-1])
 		}
+		
+	
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+	
+	loadedDeck := newDeckFromFile("_decktesting")
+	
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck 16, but got %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 }
